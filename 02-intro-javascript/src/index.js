11b511a9f88a83d48c2496aa29f547632e0129e0
Vellmond <edgar.bautista@edu.uag.mx>
2023-05-02 16:59:43 -0600

    update first codes

---
+++ 02-intro-javascript/src/index.js
@@ -0,0 +1,12 @@
+console.log('Hola Mundo');
+//variables y constantes
+const nombre = 'Edgar';
+const apellido = 'Bautista';
+let valorDado = 5;
+valorDado = 4;
+console.log(nombre, apellido, valorDado);
+if(true) {
+    let valorDado = 6;
+    console.log(valorDado);
+}
+console.log(valorDado);
